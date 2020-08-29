17b2d8a91f22ddfbe624a07af16402b900cd4f62
billash <wrash0806@my.mwsu.edu>
2020-08-28 22:54:35 -0400

    Write some tests using httr

--- tests/testthat/test-build.R
+++ tests/testthat/test-build.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(httr)
+library(tidyverse)
+library(jsonlite)
+#curl -v --header "X-TOKEN: <your_token>" https://calendly.com/api/v1/echo
+
+
+testthat::test_that('Test some basic functions using `httr`', {
+
+  # Organize  your thoughts
+
+  # test token
+  test_token <- GET('https://calendly.com/api/v1/echo',
+           httr::add_headers('X-TOKEN'=Sys.getenv("CALENDLY")))
+
+  token_resp <- fromJSON(content(test_token, 'text'))
+
+  testthat::expect_length(token_resp, 1)
+  testthat::expect_equal(token_resp[[1]], 'ash.william.r@gmail.com')
+
+
 })
--- tests/testthat/test-test-that.R
+++ tests/testthat/test-test-that.R
@@ -1,3 +1,4 @@
+# trying out the 3rd edition
 test_that("I can use the 3rd edition", {
   local_edition(3)
   expect_true(TRUE)
